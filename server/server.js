097314212b89fb1ddb6f7b3a2d7427eb76a42b0b
Pokegear <christoph.ding@gmail.com>
2016-04-19 21:33:35 -0400

    Added routing for community info page/

--- server/server.js
+++ server/server.js
@@ -9,6 +9,7 @@ var port = 8000;
 // Pages and Files
 var staticFiles = '/../client';
 var landingPage = '/../client/pages/index.html';
+var communityPage = '/../client/pages/infoPage.html';
 
 // Serve Static Files
 app.use(express.static(path.join(__dirname + staticFiles)));
@@ -17,6 +18,11 @@ app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname + landingPage));
 })
 
+app.get('/community', function(req, res) {
+  res.sendFile(path.join(__dirname + communityPage));
+})
+
+app.get('')
 // Connect to server
 app.listen(port, function() {
   console.log('Connected to server.  Listening in on port ', port)
