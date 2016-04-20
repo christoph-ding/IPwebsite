54e857db9805adc34f418a6044c1e09369e12ef3
Pokegear <christoph.ding@gmail.com>
2016-04-20 19:19:16 -0400

    Added super simple database configuration.  Working on connecting db to server now.

---
+++ server/dbConfig.js
@@ -0,0 +1,31 @@
+// Define database connection
+var Sequelize = require('sequelize');
+
+// Connection
+var connection = new Sequelize('ipAddress', null, null, {
+  dialect: 'postgres',
+  port: 5432
+})
+
+// Connect
+connection
+  .authenticate()
+  .then(function () {
+    console.log('you are now connected to IP Address')
+  })
+  .catch(function (err) {
+    console.log("something went wrong connecting to IP Address")
+  })
+
+// Ip Addresses
+var User = connection.define('User', {
+  email: Sequelize.STRING,
+  IPAddress: Sequelize.STRING,
+  Place: Sequelize.STRING
+  {
+    tableName: 'Users'
+  }
+});
+
+// Create tables
+User.sync();
